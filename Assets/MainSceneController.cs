e08071774425c67cbac42d59017daa60e34c2319
Jungho Park <haplane10>
2021-04-14 01:10:19 +0900

    day1

---
+++ Assets/MainSceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneController : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private InputField idInputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckID()
+    {
+        if (idInputField.text == string.Empty)
+        {
+            startButton.interactable = false;
+        }
+        else
+        {
+            startButton.interactable = true;
+        }
+
+    //    GameManager.Instance.Report.ID = idInputField.text;
+    }
+}
